e6d870ee8e0358dc02e635940d500a6d772a2670
anupamamaria <anupamamariabiju@gmail.com>
2025-05-23 15:53:02 +0530

    redis caching added

--- src/main/java/com/avis/vehicle_reservation_consumer/VehicleReservationConsumerApplication.java
+++ src/main/java/com/avis/vehicle_reservation_consumer/VehicleReservationConsumerApplication.java
@@ -2,8 +2,10 @@ package com.avis.vehicle_reservation_consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class VehicleReservationConsumerApplication {
 
 	public static void main(String[] args) {
